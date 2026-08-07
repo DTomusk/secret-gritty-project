57494c8e3f3cf40329825f9a1ce053c19cc3b147
Dave <david.tomusk@gmail.com>
2026-08-07 19:53:18 +0100

    Seed single admin user if database has no roles and no users

--- server/Infrastructure/Shared/DatabaseSeeder.cs
+++ server/Infrastructure/Shared/DatabaseSeeder.cs
@@ -18,12 +18,26 @@ public class DatabaseSeeder
         if (await _context.Roles.AnyAsync())
             return;
 
-        var roles = new List<Role>
-        {
-            new Role { Name = "Admin" },
-        };
-        _context.Roles.AddRange(roles);
+        var adminRole = new Role { Id = Guid.NewGuid(), Name = "Admin" };
+        _context.Roles.Add(adminRole);
         await _context.SaveChangesAsync();
 
+        // Seed test admin user if there are no users
+        if (await _context.Users.AnyAsync())
+            return;
+
+        var testAdmin = User.Create("David");
+        testAdmin.ActivateUser("$2a$11$hhBSsPx0fKrtGEKLB9JryeYCfrl940kRxnaRq4yLrY9Uxc46QVdbS");
+
+        _context.Add(testAdmin);
+        await _context.SaveChangesAsync();
+
+        var adminUserRoleResult = UserRole.Create(testAdmin.Id, adminRole.Id);
+        if (adminUserRoleResult.IsFailure)
+        {
+            throw new Exception($"Failed to create UserRole: {adminUserRoleResult.Error}");
+        }
+        _context.UserRoles.Add(adminUserRoleResult.Value);
+        await _context.SaveChangesAsync();
     }
 }
