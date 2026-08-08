e459a1ea7e57cc4545b97b90b5584c2fdf16d730
Dave <david.tomusk@gmail.com>
2026-08-08 15:45:12 +0100

    Add calendar event entity

---
+++ server/Domain.UnitTests/CalendarEvents/Entities/CalendarEventTests.cs
@@ -0,0 +1,38 @@
+﻿using Domain.CalendarEvents.Entities;
+using Xunit;
+
+namespace Domain.UnitTests.CalendarEvents.Entities;
+
+public class CalendarEventTests
+{
+    [Fact]
+    public void Create_ShouldReturnCalendarEventWithCorrectProperties()
+    {
+        // Arrange
+        var name = "Test Event";
+        var date = new DateOnly(2024, 6, 1);
+        var eventType = CalendarEventType.BookClub;
+
+        // Act
+        var calendarEvent = CalendarEvent.Create(name, date, eventType);
+
+        // Assert
+        Assert.NotEqual(Guid.Empty, calendarEvent.Id);
+        Assert.Equal(name, calendarEvent.Name);
+        Assert.Equal(date, calendarEvent.Date);
+        Assert.Equal(eventType, calendarEvent.EventType);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Create_ShouldThrowArgumentException_WhenNameIsNullOrEmpty(string name)
+    {
+        // Arrange
+        var date = new DateOnly(2024, 6, 1);
+        var eventType = CalendarEventType.BookClub;
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => CalendarEvent.Create(name, date, eventType));
+    }
+}
---
+++ server/Domain/CalendarEvents/Entities/CalendarEvent.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.CalendarEvents.Entities;
+
+public class CalendarEvent
+{
+    public Guid Id { get; init; }
+    public string Name { get; private set; }
+    public DateOnly Date { get; private set; }
+    public CalendarEventType EventType { get; private set; }
+
+    private CalendarEvent() { }
+
+    public static CalendarEvent Create(string name, DateOnly date, CalendarEventType eventType)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot be null or whitespace.", nameof(name));
+        }
+
+        return new CalendarEvent
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Date = date,
+            EventType = eventType
+        };
+    }
+}
+
+public enum CalendarEventType
+{
+    BookClub
+}
